c4d2e78db3d3e29d07a85d9152fa7b6e81ec168f
Liubov Koreva <LKoreva@vizrt.com>
2020-10-23 20:55:53 +0200

    Added video.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+  
 
 function App() {
   return (
     <div className="App">
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
+
+<video
+    id="my-player"
+    class="video-js"
+    controls
+    preload="auto"
+    poster="https://koreva-liubov-input-bucket.s3-us-west-2.amazonaws.com/black.png"
+    data-setup='{}'>
+  <source src="https://koreva-liubov-output-bucket.s3-us-west-2.amazonaws.com/10-sec-bunny_output.mp4" type="video/mp4"></source>
+<p class="vjs-no-js">
+    To view this video please enable JavaScript, and consider upgrading to a
+    web browser that
+      supports HTML5 video
+  </p>
+</video>
       </header>
     </div>
   );
